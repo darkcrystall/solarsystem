6f18cc2f1b152848ec36a60248d3c9d94722b643
darkcrystall <103094217+darkcrystall@users.noreply.github.com>
2024-06-08 23:16:13 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar")
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function() {
+    const isTheLast = cartaoAtual === cartoes.length - 1
+    if(isTheLast) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+
+    mostrarCartao();
+
+});
+
+btnVoltar.addEventListener("click", function() {
+
+    const isTheFirst = cartaoAtual === 0
+    if(isTheFirst) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+
+    mostrarCartao();
+
+});
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+
+    cartaoSelecionado.classList.remove("selecionado");
+}
